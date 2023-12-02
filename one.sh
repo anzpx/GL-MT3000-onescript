16d5af65092e28772394b1766dac2bacf4950a5f
Xiang <zpx1986@126.com>
2023-12-02 21:59:19 +0800

    j

--- one.sh
+++ one.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 # 第三方软件仓库
-#third_party_source="https://op.dllkids.xyz/packages/aarch64_cortex-a53"  只能装passwall2
+# 只能装passwall2
+#third_party_source="https://op.dllkids.xyz/packages/aarch64_cortex-a53"
+# 只能装passwall
 third_party_source="https://dl.openwrt.ai/packages-23.05/aarch64_cortex-a53/kiddin9"
 
 #添加出处信息
@@ -52,47 +54,47 @@ setup_base_init() {
 
 # 判断系统是否为iStoreOS
 is_iStoreOS() {
-	DISTRIB_ID=$(cat /etc/openwrt_release | grep "DISTRIB_ID" | cut -d "'" -f 2)
-	# 检查DISTRIB_ID的值是否等于'iStoreOS'
-	if [ "$DISTRIB_ID" = "iStoreOS" ]; then
-		return 0 # true
-	else
-		return 1 # false
-	fi
+   DISTRIB_ID=$(cat /etc/openwrt_release | grep "DISTRIB_ID" | cut -d "'" -f 2)
+   # 检查DISTRIB_ID的值是否等于'iStoreOS'
+   if [ "$DISTRIB_ID" = "iStoreOS" ]; then
+      return 0 # true
+   else
+      return 1 # false
+   fi
 }
 
 ## 去除opkg签名
 remove_check_signature_option() {
-	local opkg_conf="/etc/opkg.conf"
-	sed -i '/option check_signature/d' "$opkg_conf"
+   local opkg_conf="/etc/opkg.conf"
+   sed -i '/option check_signature/d' "$opkg_conf"
 }
 
 ## 添加opkg签名
 add_check_signature_option() {
-	local opkg_conf="/etc/opkg.conf"
-	echo "option check_signature 1" >>"$opkg_conf"
+   local opkg_conf="/etc/opkg.conf"
+   echo "option check_signature 1" >>"$opkg_conf"
 }
 
 #设置第三方软件源
 setup_software_source() {
-	## 传入0和1 分别代表原始和第三方软件源
-	if [ "$1" -eq 0 ]; then
-		echo "# add your custom package feeds here" >/etc/opkg/customfeeds.conf
-		##如果是iStoreOS系统,还原软件源之后，要添加签名
-		if is_iStoreOS; then
-			add_check_signature_option
-		else
-			echo
-		fi
-	elif [ "$1" -eq 1 ]; then
-		#传入1 代表设置第三方软件源 先要删掉签名
-		remove_check_signature_option
-		# 先删除再添加以免重复
-		echo "# add your custom package feeds here" >/etc/opkg/customfeeds.conf
-		echo "src/gz third_party_source $third_party_source" >>/etc/opkg/customfeeds.conf
-	else
-		echo "无效选项。请提供0或1。"
-	fi
+   ## 传入0和1 分别代表原始和第三方软件源
+   if [ "$1" -eq 0 ]; then
+      echo "# add your custom package feeds here" >/etc/opkg/customfeeds.conf
+      ##如果是iStoreOS系统,还原软件源之后，要添加签名
+      if is_iStoreOS; then
+         add_check_signature_option
+      else
+         echo
+      fi
+   elif [ "$1" -eq 1 ]; then
+      #传入1 代表设置第三方软件源 先要删掉签名
+      remove_check_signature_option
+      # 先删除再添加以免重复
+      echo "# add your custom package feeds here" >/etc/opkg/customfeeds.conf
+      echo "src/gz third_party_source $third_party_source" >>/etc/opkg/customfeeds.conf
+   else
+      echo "无效选项。请提供0或1。"
+   fi
 }
 
 # 下载和安装依赖
@@ -106,7 +108,7 @@ do_install_depends_ipk() {
    echo
 
    echo "开始安装必要依赖"
-	opkg install "/tmp/iptables-mod-socket_0.00-0_all.ipk"
+   opkg install "/tmp/iptables-mod-socket_0.00-0_all.ipk"
    opkg install "/tmp/kmod-inet-diag_0.00-0_all.ipk"
    opkg install "/tmp/libopenssl3.ipk"
    opkg install "/tmp/luci-lua-runtime_all.ipk"
@@ -116,29 +118,27 @@ do_install_depends_ipk() {
 
 #单独安装argon主题
 do_install_argon_skin() {
-	echo "正在尝试安装argon主题......."
-	#opkg install luci-app-argon-config
+   echo "正在尝试安装argon主题......."
+   #opkg install luci-app-argon-config
    opkg install luci-theme-edge
-	# 检查上一个命令的返回值
-	if [ $? -eq 0 ]; then
-		echo "argon主题 安装成功"
-		# 设置主题和语言
-		uci set luci.main.mediaurlbase='/luci-static/edge'
-		uci set luci.main.lang='zh_cn'
-		uci commit
-		echo "重新登录web页面后, 查看新主题 "
-	else
-		echo "argon主题 安装失败! 建议再执行一次!再给我一个机会!事不过三!"
-	fi
+   # 检查上一个命令的返回值
+   if [ $? -eq 0 ]; then
+      echo "argon主题 安装成功"
+      # 设置主题和语言
+      uci set luci.main.mediaurlbase='/luci-static/edge'
+      uci set luci.main.lang='zh_cn'
+      uci commit
+      echo "重新登录web页面后, 查看新主题 "
+   else
+      echo "argon主题 安装失败! 建议再执行一次!再给我一个机会!事不过三!"
+   fi
    echo
 }
 
-
-
 #安装首页风格
 do_install_luci_app_quickstart() {
-	opkg install luci-app-quickstart
-	echo "首页样式已经更新,请强制刷新网页,检查是否为中文字体"
+   opkg install luci-app-quickstart
+   echo "首页样式已经更新,请强制刷新网页,检查是否为中文字体"
 }
 
 # 安装首页及其所需软件
@@ -146,7 +146,7 @@ install_istore_os_style() {
 
    echo "安装首页风格"
    opkg install luci-app-quickstart
-	echo
+   echo
 
    echo "安装ddnsto app-meta-ddnsto"
    sh -c "$(wget --no-check-certificate -qO- http://fw.koolcenter.com/binary/ddnsto/openwrt/install_ddnsto.sh)"
@@ -178,67 +178,70 @@ install_istore_os_style() {
    #    brew install jq
    # fi
 
- 
-  
    # samba4
-   
+
    # 带宽监控
    # Homebox 插件
    # SysTools 插件
    # feed
 
-
    # Transmission 插件
    # qBittorrent 插件
 
-
-	# 若已安装iStore商店则在概览中追加iStore字样
-	# if ! grep -q " like iStoreOS" /tmp/sysinfo/model; then
-	# 	sed -i '1s/$/ like iStoreOS/' /tmp/sysinfo/model
-	# fi
+   # 若已安装iStore商店则在概览中追加iStore字样
+   # if ! grep -q " like iStoreOS" /tmp/sysinfo/model; then
+   # 	sed -i '1s/$/ like iStoreOS/' /tmp/sysinfo/model
+   # fi
 }
 
-do_install_passwall(){
+do_install_passwall() {
    echo "开始安装passwall......"
    opkg install luci-app-passwall
    echo
 }
 
-do_install_passwall2(){
+do_install_passwall2() {
    echo "开始安装passwall2......"
    opkg install luci-app-passwall2
    echo
 }
 
+do_install_app() {
+
+   #基础必备设置
+   setup_base_init
+   #设置第三方软件仓库
+   setup_software_source 1
+   # 设置第三方源后要更新
+   echo "更新软件包列表"
+   opkg update
+   echo
+   #下载和安装必须的依赖
+   do_install_depends_ipk
+   #设置Argon 紫色主题
+   do_install_argon_skin
+
+   # 安装首页及其所需软件
+   # install_istore_os_style
+
+   # 安装passwall
+   do_install_passwall
+   # 安装passwall2
+   do_install_passwall2
+
+   #再次更新 防止出现汉化不完整
+   #do_install_luci_app_quickstart
+
+   #恢复第三方软件仓库
+   #setup_software_source 0
+   # echo "更新软件包列表"
+   # opkg update
+   # echo
+}
+
 echo "开始安装......"
 echo
 
-#基础必备设置
-setup_base_init
-#设置第三方软件仓库
-setup_software_source 1		
-# 设置第三方源后要更新
-echo "更新软件包列表"
-opkg update
-echo
-#下载和安装必须的依赖
-do_install_depends_ipk
-#设置Argon 紫色主题
-do_install_argon_skin
-
-# 安装首页及其所需软件
-# install_istore_os_style
-
-# 安装passwall
-do_install_passwall
-# 安装passwall2
-do_install_passwall2
-
-#再次更新 防止出现汉化不完整
-#do_install_luci_app_quickstart
-
-#恢复第三方软件仓库
-#setup_software_source 0
-# echo "更新软件包列表"
-# opkg update
-# echo
+#cd /tmp
+#curl -Ls https://api.github.com/repos/xenolf/lego/releases/latest | grep browser_download_url | grep linux_amd64 | cut -d '"' -f 4 | wget -i -
+curl -Ls https://api.github.com/repos/xiaorouji/openwrt-passwall/releases/latest | grep browser_download_url
